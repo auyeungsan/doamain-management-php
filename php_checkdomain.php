f79aa4d2665edaec96af97b3a8c32e667d6ad3d5
auyeungsan <auyeungsan@gmail.com>
2016-06-14 15:05:39 +0800

    revise version

--- php_checkdomain.php
+++ php_checkdomain.php
@@ -1,30 +1,145 @@
 <?php
 
 $hosts = $_GET['domain'];
-$ex_date = $_GET['ex_date'];
-$date_left = $_GET['date_left'];
-$registar = $_GET['registar'];
+$type = $_GET['type'];
+#$ex_date = $_GET['ex_date'];
+#$date_left = $_GET['date_left'];
+#$registar = $_GET['registar'];
+#$ex_var = $_GET['ex_var'];
+$g_log_path = '/tmp/game_check';
+$ng_log_path = '/tmp/game_check';
 
-if(empty($hosts)){
-	echo ' ';
-}else{
-	if(!empty($ex_date)){
-		$result = shell_exec("./domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f39");
-		#echo "/var/www/html/domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f42";
-		echo $result;
-	}elseif(!empty($date_left)){
-                $result = shell_exec("./domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f42");
-                #echo "/var/www/html/domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f42";
-                echo $result;
-	
-	}elseif(!empty($registar)){
-		$result = shell_exec("./domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f26");
-                #echo "/var/www/html/domain-check.sh -d $hosts|awk 'NR==4'| cut -d ' ' -f42";
-                echo $result;
-	}else{
-		echo ' ';
-	}
+
+if(isset($_GET['ex_date'])){
+	$ex_date = $_GET['ex_date'];
 }
 
+if(isset($_GET['date_left'])){
+	$date_left = $_GET['date_left'];
+}
+
+if(isset($_GET['registar'])){
+	$registar = $_GET['registar'];
+}
+
+if(isset($_GET['ex_var'])){
+	$ex_var = $_GET['ex_var'];
+}
+
+
+
+if(!empty($hosts) && !empty($type)){
+
+
+	if($type == 'game'){
+	
+		if(!empty($ex_date)){
+			$result = shell_exec("grep -w $hosts $g_log_path|awk -F' ' '{ print $4 }'");
+			echo $result;
+
+		}
+	
+		if(!empty($date_left)){
+                	$result = shell_exec("grep -w $hosts $g_log_path|awk -F' ' '{ print $5 }'");
+                	echo $result;
+	
+		}
+	
+		if(!empty($registar)){
+			$result = shell_exec("grep -w $hosts $g_log_path|awk -F' ' '{ print $2 }'");
+                	echo $result;
+		}
+	
+		if(!empty($ex_var)){
+			$result = shell_exec("grep -w $hosts $g_log_path|awk -F' ' '{ print $5 }'");
+						
+			$result = trim($result);
+		
+			if(!eregi("^[0-9]+$",$result)){
+				echo 'unknow';
+				#echo $result;
+				exit;	
+			}elseif(empty($result)){
+				echo 'unknow';
+				exit;	
+			}elseif($result =='whois'){
+				echo 'unknow';
+				exit;	
+			}	
+	
+			$result = intval($result);
+
+			if($result < 7){
+
+				echo 'critical';
+				#echo $result;
+			}elseif($result <= 30){
+
+				echo 'warning';
+				#echo $result;
+			}elseif($result > 30){
+
+				echo 'normal';
+				#echo $result;
+
+			}
+
+		}
+	
+	}else{
+		if(!empty($ex_date)){
+                        $result = shell_exec("grep -w $hosts $ng_log_path|awk -F' ' '{ print $4 }'");
+                        echo $result;
+
+                }
+        
+                if(!empty($date_left)){
+                        $result = shell_exec("grep -w $hosts $ng_log_path|awk -F' ' '{ print $5 }'");
+                        echo $result;
+        
+                }
+        
+                if(!empty($registar)){
+                        $result = shell_exec("grep -w $hosts $ng_log_path|awk -F' ' '{ print $2 }'");
+                        echo $result;
+                }
+        
+                if(!empty($ex_var)){
+                        $result = shell_exec("grep -w $hosts $ng_log_path|awk -F' ' '{ print $5 }'");
+                
+			$result = trim($result);
+
+                        if(!eregi("^[0-9]+$",$result)){
+                                echo 'unknow';
+                                exit;
+                        }elseif(empty($result)){
+                                echo 'unknow';
+                                exit;
+                        }elseif($result == 'whois'){
+                                echo 'unknow';
+                                exit;
+                        }
+        
+                        $result = intval($result);
+
+                        if($result < 7){
+
+                                echo 'critical';
+
+                        }elseif($result <= 30){
+
+                                echo 'warning';
+
+                        }elseif($result > 30){
+
+                                echo 'normal';
+
+                        }
+
+                }
+
+	}	
+
+}
 
 ?>
